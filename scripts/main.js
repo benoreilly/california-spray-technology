377e1ee53409d64642ddea3cd3d88fd9d6b1f469
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-07-19 23:54:09 -0400

    cta btn update

--- scripts/main.js
+++ scripts/main.js
@@ -17,9 +17,5 @@ $(document).ready(function(){
         $("html, body").animate({ scrollTop: 0 }, 600); 
         return false; 
     });
-    $('#logo').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
 });
 
